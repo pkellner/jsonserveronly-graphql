7e738fe015c204cf11e15977e1fde63cda53c5fe
pkellner <peter@peterkellner.net>
2020-09-04 16:08:36 -0700

    working

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const jsonServer = require('json-server');
 const { BlobServiceClient } = require('@azure/storage-blob');
 
+const DELAYINMS = process.env.DELAYINMS || 5; // very short by default
+
 const server = jsonServer.create();
 // const router = jsonServer.router(
 //   "https://psgraphqlstorage.blob.core.windows.net/testcontainer/db.json"
 // );
-const router = jsonServer.router();
+const router = jsonServer.router("db.json");
+server.use(function(req, res, next) {
+  setTimeout(next, DELAYINMS);
+});
 
 const middlewares = jsonServer.defaults();
 //https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-nodejs#upload-blobs-to-a-container
@@ -13,6 +18,9 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(5000, () => {
-  console.log('JSON Server is running on port 5000');
-});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+}); 
